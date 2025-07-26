52bce4cf078d48014d5ab718464ad7c1d6d711ab
deeppatel-dev <99211241+deeppatel-dev@users.noreply.github.com>
2025-07-26 15:23:17 -0700

    Project object creation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import Task from "./task";
 import Project from "./project";
 import { projects } from "./storage.js";
+import { setupProjectForm } from "./ui.js";
 
 console.log("test");
 
@@ -9,24 +10,10 @@ let projectOne = new Project("example");
 
 projects.push(projectOne);
 
-projectOne.addTask(
-  "Example Task",
-  "This is an Example Task",
-  "9/15/25",
-  "Low",
-  false,
-);
-
-projectOne.addTask(
-  "example two",
-  "This is another example task",
-  "9/16/25",
-  "High",
-  false,
-);
-
 for (const project of projects) {
   for (const task of project.tasks) {
     console.log(task);
   }
 }
+
+setupProjectForm();
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,18 @@
 import Task from "./task";
 import Project from "./project";
 import { projects } from "./storage.js";
+
+function setupProjectForm() {
+  const projectInput = document.querySelector("#projectName");
+  const projectForm = document.querySelector(".sidebar form");
+  projectForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = projectInput.value;
+    let newProject = new Project(name);
+    projectInput.value = "";
+    projects.push(newProject);
+    console.log(projects);
+  });
+}
+
+export { setupProjectForm };
