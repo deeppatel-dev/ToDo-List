da347103de8626da77ecd3989f7efd4096343fb2
deeppatel-dev <99211241+deeppatel-dev@users.noreply.github.com>
2025-07-28 10:22:39 -0700

    Added the ability to select projects, and also update the UI to highlight the selected project

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import "./style.css";
 import Task from "./task";
 import Project from "./project";
 import { projects } from "./storage.js";
-import { setupProjectForm } from "./ui.js";
+import { setupProjectForm, selectProject } from "./ui.js";
 
 console.log("test");
 
 setupProjectForm();
+selectProject();
--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,8 @@ import Task from "./task";
 import Project from "./project";
 import { projects } from "./storage.js";
 
+let selectedProject;
+
 function setupProjectForm() {
   const projectInput = document.querySelector("#projectName");
   const projectForm = document.querySelector(".sidebar form");
@@ -14,6 +16,32 @@ function setupProjectForm() {
     console.log(projects);
 
     renderProjects();
+  });
+}
+
+function selectProject() {
+  const projectList = document.querySelector(".project-list");
+
+  projectList.addEventListener("click", (e) => {
+    if (e.target.classList[0] === "project-list") {
+      return;
+    }
+    const projectsList = document.querySelectorAll(".project-list li");
+
+    for (const element of projectsList) {
+      element.classList.remove("active");
+    }
+
+    const projectId = e.target.dataset.id;
+    e.target.classList.add("active");
+    for (const project of projects) {
+      if (project.id === projectId) {
+        console.log(`The Project Is: ${project.name}`);
+        selectedProject = project;
+        console.log(selectedProject);
+      }
+    }
+    // renderTasks(selectedProject);
   });
 }
 
@@ -33,4 +61,4 @@ function renderProjects() {
   }
 }
 
-export { setupProjectForm };
+export { setupProjectForm, renderProjects, selectedProject, selectProject };
